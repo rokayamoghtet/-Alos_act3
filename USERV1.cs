2bc9f23ae7dcd5e7dd22db674afa025287bec089
ROKAYA <101093188+rokayamoghtet@users.noreply.github.com>
2022-04-26 23:44:24 +0100

    Models

---
+++ USERV1.cs
@@ -0,0 +1,14 @@
+namespace DelaiLivraisonAPI.Models
+{
+     public class UserV1
+    {
+
+        public int Track { get; set; }
+        public string ServiceUPS { get; internal set; }
+        public string Poids { get; internal set; }
+        public string Adresse { get; internal set; }
+        public string DateLivré { get; internal set; }
+        public string TempsLivré { get; internal set; }
+        public string Signé { get; internal set; }
+    }
+}
